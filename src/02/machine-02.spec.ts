ee398c7bd0e40e30939f3f75c037cc013b29577f
krambono <kevin.rambonona@gmail.com>
2024-11-12 22:01:57 +0100

    refactor machine 02

--- src/02/machine-02.spec.ts
+++ src/02/machine-02.spec.ts
@@ -4,16 +4,19 @@ import { Dependencies } from './dependencies';
 import { createMachine02 } from './machine-02';
 
 describe('machine 02', () => {
+  const dependencies = {
+    fetchData: jest.fn(),
+  } satisfies Dependencies;
   const id = 'my-id';
+
+  beforeEach(() => {
+    dependencies.fetchData.mockClear();
+  });
 
   describe('Fetching', () => {
     it("should start in 'Fetching' state", () => {
       // given
-      const dependencies = {
-        fetchData: jest.fn(),
-      } satisfies Dependencies;
-      const machine = createMachine02(dependencies);
-      const actor = createActor(machine, { input: { id } });
+      const actor = initializeActor();
 
       // when
       actor.start();
@@ -26,11 +29,8 @@ describe('machine 02', () => {
 
     it("should transition to 'Done' state when the fetching of data is done ", async () => {
       // given
-      const dependencies = {
-        fetchData: jest.fn().mockResolvedValue({ content: 'my-content' }),
-      } satisfies Dependencies;
-      const machine = createMachine02(dependencies);
-      const actor = createActor(machine, { input: { id } });
+      dependencies.fetchData.mockResolvedValue({ content: 'my-content' });
+      const actor = initializeActor();
       actor.start();
 
       // when
@@ -45,11 +45,8 @@ describe('machine 02', () => {
 
     it("should transition to 'Error' state when the fetching of data fails", async () => {
       // given
-      const dependencies = {
-        fetchData: jest.fn().mockRejectedValue(new Error('An error occurred')),
-      } satisfies Dependencies;
-      const machine = createMachine02(dependencies);
-      const actor = createActor(machine, { input: { id } });
+      dependencies.fetchData.mockRejectedValue(new Error('An error occurred'));
+      const actor = initializeActor();
       actor.start();
 
       // when
@@ -62,4 +59,9 @@ describe('machine 02', () => {
       expect(snapshot.context.error).toStrictEqual(new Error('An error occurred'));
     });
   });
+
+  function initializeActor() {
+    const machine = createMachine02(dependencies);
+    return createActor(machine, { input: { id } });
+  }
 });
